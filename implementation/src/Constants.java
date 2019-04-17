69dde4de7385a5fa92a9ba3951751e04deb1f25a
Sofiia Materynska <materynskasofiyka@gmail.com>
2019-04-18 02:09:24 +0300

    The time step was reduced

--- implementation/src/Constants.java
+++ implementation/src/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final double c1=.5;
     public static final double c2=1.;
     public static final int numberOfXIterations=20;
-    public static final int numberOfTimeIterations=800;
+    public static final int numberOfTimeIterations=1000;
     public static final double xStep=1./numberOfXIterations;
     public static final double timeStep=1./numberOfTimeIterations;
 }
