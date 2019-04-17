c4790448dbdba51a01c62be7fd3c58b1d3405b84
Sofiia Materynska <materynskasofiyka@gmail.com>
2019-04-18 02:10:18 +0300

    Code was improved, commnts added

--- implementation/src/ExactValues.java
+++ implementation/src/ExactValues.java
@@ -1,21 +1,17 @@
+/**
+ * Визначення точного розв'язку
+ */
 public class ExactValues {
     public static void  main(String [] args){
         View view=new View();
+        double [][] matrix= new double[Constants.numberOfTimeIterations+1][Constants.numberOfXIterations+1];
         for(int t=0; t<Constants.numberOfTimeIterations+1; t++){
             for(int i=0; i<Constants.numberOfXIterations+1; i++){
-                view.printFormatted("%.2f",Math.pow(Constants.c0*Constants.xStep*i-
-                        Constants.a*Constants.c1*t*Constants.timeStep+Constants.c2, -1));
-                view.print(" ");
+                matrix[t][i]= Math.pow(Constants.c0 * Constants.xStep * i -
+                        Constants.a * Constants.c1 * t * Constants.timeStep + Constants.c2, -1);
             }
-            view.print("\n");
         }
-//        for(int t=0; t<Constants.numberOfTimeIterations; t++) {
-//            for (int i = 0; i < Constants.numberOfXIterations; i++) {
-//                view.print(System.out.print("{" + Constants.xStep * i + "," + Constants.timeStep * t + ","
-//                        + Math.pow(Constants.c0 * Constants.xStep * i -
-//                        Constants.a * Constants.c1 * t * Constants.timeStep + Constants.c2, -1) + "},");
-//            }
-//            view.print("\n");
-//        }
+        view.printArray(matrix);
+        view.printToPlot(matrix);
     }
 }
--- implementation/src/SequentialSolving.java
+++ implementation/src/SequentialSolving.java
@@ -1,28 +1,32 @@
+/**
+ * Послідовна реалізація явного методу
+ */
 public class SequentialSolving {
     public static void main(String[] args) {
         double[][] matrix = new double[Constants.numberOfTimeIterations+1][Constants.numberOfXIterations+1];
         View view=new View();
-        //
+        //початкові умови t=0
         for (int i = 0; i < Constants.numberOfXIterations+1; i++) {
             matrix[0][i] = Math.pow(Constants.c0 * Constants.xStep * i + Constants.c2, -1);
         }
+        //граничні умови x=0, x=1
         for (int t = 1; t < Constants.numberOfTimeIterations+1; t++) {
             matrix[t][0] = Math.pow(-Constants.a * Constants.c1 * t * Constants.timeStep + Constants.c2, -1);
             matrix[t][Constants.numberOfXIterations] = Math.pow(Constants.c0 -
                     Constants.a * Constants.c1 * t * Constants.timeStep + Constants.c2, -1);
         }
+        //всі інші, крім крайових значень розв'язку за виведеною вручну формулою
         for (int t = 0; t < Constants.numberOfTimeIterations ; t++) {
             for (int i = 1; i < Constants.numberOfXIterations ; i++) {
-                matrix[t + 1][i] = Constants.a * Constants.timeStep * (2 * matrix[t][i] * (matrix[t][i - 1] - 2 * matrix[t][i] + matrix[t][i + 1])
-                        - Constants.xStep * Math.pow(matrix[t][i + 1] - matrix[t][i - 1], 2)) / (2 * Math.pow(Constants.xStep * matrix[t][i], 2)) + matrix[t][i];
+                matrix[t + 1][i] = Constants.a * Constants.timeStep * (2 * matrix[t][i]
+                        * (matrix[t][i - 1] - 2 * matrix[t][i] + matrix[t][i + 1])
+                        - Constants.xStep * Math.pow(matrix[t][i + 1] - matrix[t][i - 1], 2))
+                        / (2 * Math.pow(Constants.xStep * matrix[t][i], 2)) + matrix[t][i];
             }
         }
-        for (int t = 0; t < Constants.numberOfTimeIterations+1 ; t++) {
-            for (int i = 0; i < Constants.numberOfXIterations+1 ; i++) {
-                view.printFormatted("%.2f",matrix[t][i]);
-                view.print(" ");
-            }
-            view.print("\n");
-        }
+        //вивід результату на екран
+        view.printArray(matrix);
+        //вивід результуту для побудови графіку
+        view.printToPlot(matrix);
     }
 }
